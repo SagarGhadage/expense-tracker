8916473d8e3983aa7cf0c306c2758c334b9d0299
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-04-20 23:37:28 +0530

    Comps structure Implemented

---
+++ src/Components/Graph/Graph.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class Graph extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>Graph</div>
+    )
+  }
+}
+
+export default Graph
---
+++ src/Components/Pie/Pie.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './Pie.module.css'
+function Pie() {
+  return (
+    <div className={style.pie}>
+      <div className={style.list}>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Pie
