eac9ddc8ebfc73c0bab5ac6f6242deb0b87ddfb6
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-04-20 23:32:08 +0530

    Button implemented

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import style from "./Button.module.css"
+function Button({ varient, handleClick, btnText, children }) {
+  console.log(varient)
+  switch (varient) {
+    case btnVarient.GREEN:
+      return (
+        <button className={style.GREEN} onClick={handleClick}>{btnText ? btnText : children ? children : "Button"} </button>
+      )
+    case btnVarient.RED: return (
+      <button className={style.RED} onClick={handleClick}>{btnText ? btnText : children ? children : "Button"} </button>
+    )
+    case btnVarient.YELLOW:
+      return (
+        <button className={style.YELLOW} onClick={handleClick}>{btnText ? btnText : children ? children : "Button"} </button>
+      )
+    case btnVarient.LITEGREY:
+      return (
+        <button className={style.LITEGREY} onClick={handleClick}>{btnText ? btnText : children ? children : "Button"} </button>
+      )
+    default: return (
+      <button className={style.defaultBtn} onClick={handleClick}>{btnText ? btnText : children ? children : "Button"} </button>
+    ) 
+  }
+
+}
+export const btnVarient = Object.freeze({
+  YELLOW: "YELLOW", RED: "RED", GREEN: "GREEN", LITEGREY: "LIGHTGREY",
+})
+export default Button
