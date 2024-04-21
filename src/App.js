9da55a8fd3bf80037b435d2c8a79a2f957cb0bf4
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-04-21 19:13:20 +0530

    Comps structure Implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import Section from './Components/Section/Section';
-
+import Section, { sectionType } from './Components/Section/Section';
+import Card, { cardType } from './Components/Card/Card';
+import Pie from './Components/Pie/Pie';
+import Graph from './Components/Graph/Graph';
+import Pagination from './Components/Pagination/Pagination';
+import PieC from './Components/Pie/Pie';
+import Indicator from './Components/Indicator/Indicator';
+import { data } from './data';
 function App() {
   return (
     <div className="App">
       <h1 className='App-header'>Expense Tracker</h1>
-      <Section></Section>
+      <Section varient={sectionType.BG_Grey}>
+        <Card heading={"Wallet Balance"} value={"₹4500"} type={cardType.Balance} handleClick={"addBalance"} ></Card>
+        <Card heading={"Expences"} value={"₹500"} type={cardType.Expences} handleClick={"addExpense"}></Card>
+        <PieC >
+          <Indicator data={[{ text: "Food", color: "#A000FF" }, { text: "Inter", color: "#FF9304" }]}></Indicator>
+        </PieC>
+      </Section>
+      <Section>
+        <Pagination heading={"Recent Transactions"}>
+        </Pagination>
+        <Graph heading={"Top Expense"} data={data}>
+        </Graph>
+      </Section>
     </div>
   );
 }
