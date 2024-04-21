0f05639e5ab918390b4323afe6c1472cacd0d69c
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-04-21 23:46:53 +0530

    Data

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const data = [
+    {
+        catagory: "Food",
+        value: 68,
+    },
+    {
+        catagory: "Entertainment",
+        value: 10
+    }, 
+    {
+        catagory: "Travel",
+        value: 100,
+    },
+    
+];
+
+export const dataMain = [
+    {
+        id:0,
+        title:"",
+        amount:0,
+        catagory: "Food",
+        date:""
+        // value: 20,
+    },
+    {
+        catagory: "Entertainment",
+        value: 30
+    }, 
+    {
+        catagory: "Travel",
+        value: 10,
+    },
+    
+];
+
