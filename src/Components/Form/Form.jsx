5674ccd924218c5ffa2a7565b5d02a4a844bf81d
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-04-28 19:38:43 +0530

    Form Implemented

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,9 +1,164 @@
 import React from 'react'
+import style from './form.module.css'
+import { categories } from '../../data';
+import Button, { btnVarient } from '../Button/Button';
+function Form({ children, data, type, handleSubmit, setData, handleCancel }) {
+  const handleInputChange = (e) => {
+    setData((prevState) => ({ ...prevState, [e.target.name]: e.target.value }));
+  };
+  switch (type) {
+    case Formtype.AddIncome: {
+      return (
+        <div>
+          <h2 className="formHeader">Add Balance</h2>
+          <form className="incomeFrom" onSubmit={handleSubmit}>
+            <input
+              type="number"
+              placeholder="Income amount"
+              className={style.input}
+              name="income"
+              value={data.income}
+              onChange={(e) => handleInputChange(e)}
+              required
+            />
+            <div className={style.btns}>
+              <Button varient={btnVarient.YELLOW} handleClick={handleSubmit} type={"submit"} >Add Balance</Button>
+              <Button
+                varient={btnVarient.LITEGREY}
+                type="reset"
+                handleClick={handleCancel}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </div>
+      )
+    }
+    case Formtype.AddExpence:
+      {
+        return <div className="">
+          <h2 className="formHeader">Add Expense</h2>
+          <form className="incomeFrom" onSubmit={handleSubmit}>
+            <input
+              name="title"
+              placeholder="Title"
+              value={data.title}
+              onChange={handleInputChange}
+              requireds
+            />
 
-function Form() {
-  return (
-    <div>Form</div>
-  )
+            <input
+              name="amount"
+              placeholder="Price"
+              type="number"
+              value={data.amount}
+              onChange={handleInputChange}
+              required
+            />
+            <select
+              className="select"
+              name="category"
+              value={data.category}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="">Select Category</option>{" "}
+              {/* Default empty option */}
+              {categories.map((category, index) => (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+            <input
+              name="date"
+              placeholder="Date"
+              type="date"
+              value={data.date}
+              onChange={handleInputChange}
+              required
+            />
+            <div className={style.btns}>
+              <Button varient={btnVarient.YELLOW} type={"submit"}>
+                Add Expense
+              </Button>
+              <Button
+                varient={btnVarient.LITEGREY}
+                type="button"
+                handleClick={handleCancel}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </div>
+      }
+    case Formtype.EditExpence:
+      {
+        return <div className="">
+          <h2 className="formHeader">Edit Expense</h2>
+          <form className="incomeForm" onSubmit={handleSubmit}>
+            <input
+              name="title"
+              placeholder="Title"
+              value={data.title}
+              onChange={handleInputChange}
+              requireds
+            />
+
+            <input
+              name="amount"
+              placeholder="Price"
+              type="number"
+              value={data.amount}
+              onChange={handleInputChange}
+              required
+            />
+            <select
+              className="select"
+              name="category"
+              value={data.category}
+              onChange={handleInputChange}
+              required
+            >
+              <option value=''>Select Category</option>{" "}
+              {/* Default empty option */}
+              {categories.map((category, index) => (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+            <input
+              name="date"
+              placeholder="Date"
+              type="date"
+              value={data.date}
+              onChange={handleInputChange}
+              required
+            />
+            <div className={style.btns}>
+              <Button varient={btnVarient.YELLOW}  type={"submit"}>Save</Button>
+              <Button
+                varient={btnVarient.LITEGREY}
+                type="button"
+                handleClick={handleCancel}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </div>
+      }
+    default: {
+      return <>Form</>
+    }
+  }
 }
-
+export const Formtype = Object.freeze({
+  AddIncome: 'AddIncome',
+  AddExpence: 'AddExpence',
+  EditExpence: 'EditExpense',
+})
 export default Form
