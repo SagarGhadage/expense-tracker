e23a359782eb5fedde378f3ea1dd3710dcfc65b5
SagarGhadage <135639516+SagarGhadage@users.noreply.github.com>
2024-04-28 19:40:27 +0530

    Pagination Implemented

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import style from './Pagination.module.css'
-function Pagination({data,children,heading}) {
-  const [employees, setEmps] = useState([])
+import { ImAirplane, } from "react-icons/im";
+import { MdLocalGroceryStore } from "react-icons/md";
+import { ReactComponent as Food } from "../../assets/Pizza_light.svg";
+import { ReactComponent as Entertainment } from '../../assets/Gift_light.svg'
+import { ReactComponent as Arrow_Left } from "../../assets/Arrow_alt_left_alt.svg";
+import { ReactComponent as Arrow_R } from "../../assets/Arrow_alt_lright_alt.svg"
+import { TiDeleteOutline } from "react-icons/ti";
+import { ReactComponent as TravelIcon } from '../../assets/suitcase_light (1).svg'
+import { MdOutlineModeEditOutline } from "react-icons/md";
+import { FaMoneyBillWave } from "react-icons/fa6";
+import { FcDatabase } from "react-icons/fc";
+function Pagination({ data, children, heading, onEditClick,onDeleteClick, setExpense }) {
   const [current, setCurr] = useState(1)
-  const [empPaged, setempPaged] = useState([])
+  console.log(data)
   const handlePage = (e) => {
     // let len = employees.length
     // if (current + Number(e.target.value) < 1) {
@@ -13,45 +23,69 @@ function Pagination({data,children,heading}) {
     // } else {
     //   setCurr(current + Number(e.target.value))
     // }
-    setCurr(current+Number(e.target.value))
+    setCurr(current + Number(e))
   }
-  // useEffect(() => {
-  //   axios.get('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
-  //     .then((res) => { setEmps(res.data); setempPaged(res.data.slice(0, 10)) })
-  //     .catch((e) => { console.error("failed to fetch data: ", e); alert("failed to fetch data") })
-  // }, [])
+  const icon = (catagory) => {//debugger
+    switch (catagory) {
+      case "Entertainment": { return <Entertainment /> }
+      case "Travel": { return <ImAirplane /> }
+      case "Food": { return <Food></Food> }
+      case "Shopping": { return <MdLocalGroceryStore></MdLocalGroceryStore> }
+      case "Grocery": { return <MdLocalGroceryStore /> }
+      case "Bills": { return <FaMoneyBillWave /> }
+      case "Others": { return <FcDatabase /> }
+      default: {
+        return null
+      }
+    }
+  }
   return (
     <div className={style.container}>
-        {heading&&<h1 className={style.heading}>
-          {heading}
-        </h1>}
-    <div className={style.pagination}>
-    <div className="xpcontainer">
-      {data.slice(current*10-10,current*10).map((ele) => {
-            return (
-              <div className={style.row}>
-                <hr></hr>
+      {heading && <h1 className={style.heading}>
+        {heading}
+      </h1>}
+      <div className={style.pagination}>
+        <div className={style.paginationContainer}>
+          {data.slice(current * 3 - 3, current * 3).map((ele) => {
+            return (<div key={ele.id}>
+              <div key={ele.id} className={style.row}>
+                <div className={style.title}>
+                  <div className={style.icon}>{icon(ele.category)}</div>
+                  <div className={style.titleGrp}>
+                    <div className={style.titleText}>{ele.title}</div>
+                    <div className={style.date}>{ele.date}</div>
+                  </div>
+                </div>
+                <div className={style.expenseEdit}>
+                  <div className={style.amount}> ₹{ele.amount}</div>
+                  <div className={style.delete} onClick={()=>{onDeleteClick(ele)}}><TiDeleteOutline className={style.deletIcon}></TiDeleteOutline></div>
+                  <div id={ele.id} className={style.edit} onClick={() => {
+                    onEditClick(); console.log(ele)
+                    setExpense(ele)
+                  }}><MdOutlineModeEditOutline className={style.editIcon} ></MdOutlineModeEditOutline></div>
+                </div>
               </div>
+              <hr /></div>
             )
           })}
-        
-      <div className="btngrp">
-        <button value={-1} 
-        className="xpbtn"
-          type="button"
-          name="button"
-          onClick={(e) => handlePage(e)}
-        >Previous</button>
-        <div className="PgNo">{current}</div>
-        <button value={1} 
-        className="xpbtn"
-          type="button"
-          name="button"
-          onClick={(e) => handlePage(e)}
-        >Next</button>
+          <div className={style.btngrp}>
+            <button value={-1}
+              className={style.xpbtn}
+              type="button"
+              name="button"
+              onClick={(e) => handlePage(e.currentTarget.value)}
+            ><Arrow_Left values={-1}/></button>
+            <div className={style.PgNo}>{current}</div>
+            <button value={1}
+              className={style.xpbtn}
+              type="button"
+              name="button"
+              onClick={(e) => handlePage(e.currentTarget.value)}
+            ><Arrow_R  value={1}/></button>
+          </div>
+        </div>
+        {children}
       </div>
-    </div>
-    </div>
     </div>
   )
 }
